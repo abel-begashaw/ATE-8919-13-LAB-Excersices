8be6a39e636c54875a6c3de7ecbffb614f0310ef
abel-begashaw <162702561+abel-begashaw@users.noreply.github.com>
2024-03-08 17:13:13 +0300

    Added LAB-1-Classes-and-Objects-Exercise-3-Shapes.dart

---
+++ LAB-1-Classes-and-Objects-Exercise-3-Shapes.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+abstract class Shape {
+  double calculateArea();
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double calculateArea() {
+    return pi * radius * radius;
+  }
+}
+
+class Square extends Shape {
+  double side;
+
+  Square(this.side);
+
+  @override
+  double calculateArea() {
+    return side * side;
+  }
+}
+
+void main() {
+  Circle circle = Circle(6.0);
+
+  print('Area of Circle: ${circle.calculateArea()}');
+
+  Square square = Square(17.0);
+
+  print('Area of Square: ${square.calculateArea()}');
+}
